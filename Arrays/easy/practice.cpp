fc479ddac71e172b237b799c13fe0d728fa0d00c
Sathwikcm1 <sathwikcm@gmail.com>
2024-07-05 12:52:09 +0530

    random_commit

---
+++ Arrays/easy/practice.cpp
@@ -0,0 +1,10 @@
+// this is just an example file.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+int arr[] = {1, 2, 3, 4, 5};
+int n = sizeof(arr[0])/sizeof(arr[0]);
+cout << n << endl;
+return 0;
+}
