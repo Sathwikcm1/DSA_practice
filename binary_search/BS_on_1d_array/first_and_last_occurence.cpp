750058a93747737b62739f87c257e253993aae9c
Sathwikcm1 <sathwikcm@gmail.com>
2024-07-14 23:24:00 +0530

    did nothing

---
+++ binary_search/BS_on_1d_array/first_and_last_occurence.cpp
@@ -0,0 +1,10 @@
+//! in this problem we have to find the first and last occurrence of an element in a sorted array.
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+int main() {
+    
+    return 0;
+}
