157516ce0e45f452cf147724b9e7e17759dcd147
Sathwikcm1 <sathwikcm@gmail.com>
2024-08-06 18:25:06 +0530

    i don't know man

--- binary_search/BS_on_1d_array/search_element_in_a_rotated_sorted_array.py
+++ binary_search/BS_on_1d_array/search_element_in_a_rotated_sorted_array.py
@@ -1,18 +1,18 @@
-def brute_force_approach(arr,target):
-    for i in range(len(arr)):
-        if arr[i] == target:
+def brute_force_approach(arr,target): #? this is basically the brute force apporach where we just look for the target by going through the array.
+    for i in range(len(arr)):  #? this loop will run till the length of the array
+        if arr[i] == target:   #? if the target is found then it will return the index
             return i
 
-def optimal_search(arr,target):
+def optimal_search(arr,target):  #todo this is the optimal approach, where we use binary search.
     low = 0
     high = len(arr - 1)
     while(low <= high):
-        mid = (low+high)//2
-        if arr[mid] == target :
+        mid = (low+high)//2             #todo Calculating the mid index.
+        if arr[mid] == target :         #todo if the target is found then it will return the index.
             return mid
         #? this is if left part is sorted
         if arr[low] <= arr[mid]:
-            if arr[low] <= target and target <= arr[mid]:
+            if arr[low] <= target and target <= arr[mid]:  #todo if the 
                 high = mid - 1
             else:
                 low = mid + 1
