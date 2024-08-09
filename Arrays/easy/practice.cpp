0857f1b7a507f50b78e14b227bca2c02971b1828
Sathwikcm1 <sathwikcm@gmail.com>
2024-08-09 13:08:41 +0530

    morning_session(09/08/24)

--- Arrays/easy/practice.cpp
+++ Arrays/easy/practice.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main() {
 int arr[] = {1, 2, 3, 4, 5};
 int n = sizeof(arr[0])/sizeof(arr[0]);
+vector<int> mew{3,4,3,5,6,7,7};
+int size = mew.size();
 cout << n << endl;
 cout << "Hello there this is me sathwik again , here out learning the neovim commands to make it look i'm wizard to the other people.\n";
 
