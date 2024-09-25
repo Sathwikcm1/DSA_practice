f6827a473b39a1bb98c051bbb9925310c45f9dd3
Sathwikcm1 <sathwikcm@gmail.com>
2024-09-25 11:22:34 +0530

    (25/09/24)

--- Arrays/easy/practice.cpp
+++ Arrays/easy/practice.cpp
@@ -1,12 +1,11 @@
-// this is just an example file.
 #include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-int arr[] = {1, 2, 3, 4, 5};
-int n = sizeof(arr[0])/sizeof(arr[0]);
-vector<int> mew{3,4,3,5,6,7,7};
-int size = mew.size();
-cout << n << endl;
-return 0;
+int main(){
+  vector<int> arr{1,2,3,4,4,6};
+  int n = arr.size();
+  cout << n << endl;
+  cout << "this is a test program \n";
+  cout << endl;
+  cout << "The size of the array is : " << n <<endl;
+  return 0;
 }
