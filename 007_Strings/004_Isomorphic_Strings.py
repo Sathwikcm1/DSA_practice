8ec5e24fab5cc9f2e9c351843aac38d12d9b37b6
Sathwikcm1 <sathwikcm@gmail.com>
2024-11-16 23:23:52 +0530

    004_strings

--- 007_Strings/004_Isomorphic_Strings.py
+++ 007_Strings/004_Isomorphic_Strings.py
@@ -4,26 +4,34 @@
 #   foo, bar: false.
 #   paper, title: true.
 
+#TODO: we use the concept of hasmap or lookup table.
 def isIsomorphic(s,t):
+    #NOTE: if the length of any of them is null, we return false.
     if s is None or t is None:
         return False 
+    #NOTE: or if we find any of them contains the string as "" we return false.
     elif s == "" and t == "":
         return True 
     else:
+    #NOTE: if the lenght of both the strings are not equal we return false.
         if len(s) != len(t):
             return False
+        #NOTE: initialising the lookup table or dictionary.
         lookup = {}
 
         for i in range(0,len(s)):
-            c1 = s[i]
-            c2 = t[i]
+            c1 = s[i] #NOTE: contains a character from the string s.
+            c2 = t[i] #NOTE: contains a character from the string t.
             
             if c1 in lookup:
+                #NOTE: if the c1 is present in the lookup check for it's value if it is not c2 then return False
                 if lookup[c1] != c2:
                     return False
             else:
+                #NOTE: if the c2 value is already present in the values, we return false.
                 if c2 in lookup.values():
                     return False
+                #NOTE: otherwise we add the key-value pair to the dictionary or lookup table.
                 lookup[c1] = c2
 
     return True
