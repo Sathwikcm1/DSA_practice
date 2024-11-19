ebdb750725c4babae2af179e3b132fc8e3c523d5
Sathwikcm1 <sathwikcm@gmail.com>
2024-11-19 09:36:30 +0530

    adding_sort

--- 002_Sorting_Algorithms/002_Selection_Sort.py
+++ 002_Sorting_Algorithms/002_Selection_Sort.py
@@ -1,4 +1,17 @@
 def selection_sort(arr):
     n = len(arr)
+    #NOTE: this has time complexity of O(n^2).
+    #NOTE: selects the mini element and puts it into the right spot.
+    #NOTE: the outer loop goes till n-2 only.
+    for i in range(n - 1):
+        mini = i 
+        for j in range(i+1,n):
+            if arr[j] < arr[mini]:
+                mini = j
+        arr[i],arr[mini] = arr[mini],arr[i]
 
-    for i in range()
+arr = [5,3,3,6,2,8,1]
+selection_sort(arr)
+print("The array after sorting: ")
+print(*arr)
+#NOTE: the * operator is called splat or the unpacking operator is used to unpack the elements of the list.
