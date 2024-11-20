12d83b78bee1042c2935dbf8894f911e00448d6b
Sathwikcm1 <sathwikcm@gmail.com>
2024-11-20 19:58:28 +0530

    006

---
+++ 005_Linked_List/003_Problem_On_SLL/002_Reverse_A_SLL.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,data,next=None):
+        self.data = data 
+        self.next = next
+
+def brute_force_approach(head):
+    temp = head
+    stack = []
+
+    while temp:
+        stack.append(temp.data)
+        temp=temp.next
+
+
+    temp = head
+    while temp:
+        temp.data = stack.pop()
+        temp = temp.next
+    return head
+
+def print_linked_list(head):
+    temp = head
+    while temp is not None:
+        print(temp.data, end=" ")
+        temp = temp.next
+    print()
+
+head = Node(1)
+head.next = Node(3)
+head.next.next = Node(2)
+head.next.next.next = Node(4)
+
+print("The reversed linked list using brute_force_approach.")
+brute_force_approach(head)
+print_linked_list(head)
