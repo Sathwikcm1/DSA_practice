105bdbca82ddd3b0b407d81dc41e632e59a1fd12
Sathwikcm1 <sathwikcm@gmail.com>
2024-11-21 23:49:44 +0530

    006_LL

--- 005_Linked_List/003_Problem_On_SLL/007_Remove_Nth_Node_From_Back.py
+++ 005_Linked_List/003_Problem_On_SLL/007_Remove_Nth_Node_From_Back.py
@@ -6,3 +6,53 @@ class Node:
         self.next = next
 
 #TODO: this is the brute force approach.
+# story: so first we will count the no of nodes in the linked list.
+# and then we will subtract the cnt by number. now we have to traverse to that particular result and delete that node.
+
+def brute_force(head,N):
+    if head is None:
+        return None
+    cnt = 0
+    temp = head
+
+    while temp:
+        cnt+=1
+        temp=temp.next
+    #NOTE: this is the case, where N is the first node, so we delete head and return the next node to the head.
+    if cnt == N:
+        new_head = head.next
+        head=None
+        return new_head
+    
+    res = cnt-N
+    temp = head
+    while temp:
+        res-=1
+        if res == 0:
+            break
+        temp=temp.next
+    delNode = temp.next
+    temp.next = temp.next.next
+    delNode = None
+    return head
+
+def printLL(head):
+    while head:
+        print(head.val,end = " ")
+        head=head.next
+    print()
+
+
+
+
+
+arr = [1, 2, 3, 4, 5]
+N = 3
+head = Node(arr[0])
+head.next = Node(arr[1])
+head.next.next = Node(arr[2])
+head.next.next.next = Node(arr[3])
+head.next.next.next.next = Node(arr[4])
+
+head = brute_force(head,N)
+printLL(head)
