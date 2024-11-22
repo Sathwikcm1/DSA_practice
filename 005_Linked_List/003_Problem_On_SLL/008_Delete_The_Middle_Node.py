b9de77dc279fd8ad666df4d5d32736d5738814b5
Sathwikcm1 <sathwikcm@gmail.com>
2024-11-22 20:32:40 +0530

    009

--- 005_Linked_List/003_Problem_On_SLL/008_Delete_The_Middle_Node.py
+++ 005_Linked_List/003_Problem_On_SLL/008_Delete_The_Middle_Node.py
@@ -8,5 +8,71 @@ class Node:
 
 
 #TODO: this is the brute force solution.
+#story: simple, find out the middle node using the same method of counting the nodes and then delete the middle node.
 def brute_force(head):
+    temp = head
+    cnt = 0
+    while temp:
+        cnt+=1
+        temp=temp.next
+    res = cnt//2
+    temp = head
+    while temp:
+        res-=1
+        if res == 0:
+            mid = temp.next
+            temp.next=temp.next.next
+            break
+        temp=temp.next
+    return head
+#TODO:so the time complexity is O(N) +O(N/2)
 
+def print_linked_list(head):
+    temp = head
+    while temp:
+        print(temp.val, end=" ")
+        temp = temp.next
+    print()
+
+
+
+
+#TODO: optimal approach, this is using fast and slow pointers.
+#story: this is using the same fast and slow pointers, same way we find out the middle node and delete it.
+def optimal(head):
+    if head is None or not head.next:
+        return None
+    slow=head
+    fast=head
+    #NOTE:prev : because after the while, slow pointer will be pointing to the actual middle node, so we need to get to prev node of middle in order to delete the middle node.
+    prev=None
+    
+    while fast and fast.next:
+        prev=slow
+        slow=slow.next
+        fast=fast.next.next
+    
+    if prev:
+        prev.next=slow.next
+    else:
+        head=head.next
+    return head
+
+
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+head.next.next.next.next.next = Node(6)
+# Display the original linked list
+print("Original Linked List: ", end="")
+print_linked_list(head)
+
+head = brute_force(head)
+print("Updated after brute force :",end=" ")
+print_linked_list(head)
+
+head = optimal(head)
+print("Updated after optimal: ",end =" ")
+print_linked_list(head)
---
+++ 005_Linked_List/003_Problem_On_SLL/009_Sorting_LL.py
@@ -0,0 +1,57 @@
+#FIXME: Return a sorted linked list based on the value ofcourse.
+
+class Node:
+    def __init__(self,val,next=None):
+        self.val=val
+        self.next=next
+
+#TODO: this is the brute force solution.
+#story: put all the values into an array and then sort it and put it back in the linked list.
+def brute_force(head):
+    arr = []
+    temp=head
+    while temp:
+        arr.append(temp.val)
+        temp=temp.next
+    
+    arr.sort()
+    temp= head
+    for i in range(len(arr)):
+        temp.val=arr[i]
+        temp=temp.next
+
+    return head
+
+
+
+
+
+
+
+
+
+
+
+
+def print_linked_list(head):
+    temp = head
+    while temp is not None:
+        # Print the data of the current node
+        print(temp.data, end=" ")
+        # Move to the next node
+        temp = temp.next
+    print()
+
+
+# Linked List: 3 2 5 4 1
+head = Node(3)
+head.next = Node(2)
+head.next.next = Node(5)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(1)
+
+print("Original Linked List: ", end="")
+print_linked_list(head)
+print("After brute force solution to sort the list:")
+head=brute_force(head)
+print_linked_list(head)
