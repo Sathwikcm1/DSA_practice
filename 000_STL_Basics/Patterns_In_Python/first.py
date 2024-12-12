d195f4ed89b5c21ec09d5bee3b1bf63be5e750b4
Sathwikcm1 <sathwikcm@gmail.com>
2024-12-12 13:52:15 +0530

    first_pattern

---
+++ 000_STL_Basics/Patterns_In_Python/first.py
@@ -0,0 +1,9 @@
+#FIXME: this is the first pattern that is given in the A-Z sheet by striver.
+
+for _ in range(5):
+    for k in range(5):
+        print("*",end=" ")
+    print()
+
+#NOTE: the first for loop will go vertically, for first row the inner loop will act 
+# so the means the inner loop will print * s for each row, and end with " "
