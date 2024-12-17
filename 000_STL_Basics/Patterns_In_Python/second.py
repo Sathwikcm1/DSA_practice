8a4337e1399ffdde0383f5d6d64ceb28f1b0b4f1
Sathwikcm1 <sathwikcm@gmail.com>
2024-12-17 20:55:26 +0530

    03_Patterns

--- 000_STL_Basics/Patterns_In_Python/second.py
+++ 000_STL_Basics/Patterns_In_Python/second.py
@@ -1,5 +1,12 @@
 #FIXME: this is the second pattern given in striver A-Z sheet.
 
-no = int(intput("Enter the size for pattern: "))
-for _ in range(): 
-    
+no = int(input("Enter the size for pattern: "))
+for i in range(no,0,-1):
+    print("* " * (i))
+
+#NOTE: this will print the ulta triangle , 
+#note: the python for loop , in range function we have got three parameters, range(start,end, step)
+#start represents where the loop starts from, by default it is zero.
+#end represents where the loop ends, and it is excluding the end(no)
+#step, if negative it will count backwords, otherwise it will go from start.
+
