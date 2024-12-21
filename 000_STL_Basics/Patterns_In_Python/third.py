436b29d238630754d5557fa6c7f6b127c2002588
Sathwikcm1 <sathwikcm@gmail.com>
2024-12-21 22:47:49 +0530

    nochanges

--- 000_STL_Basics/Patterns_In_Python/third.py
+++ 000_STL_Basics/Patterns_In_Python/third.py
@@ -5,5 +5,4 @@ for i in range(no):
     print("* " * i)
 
 #TODO: This will print the "*"s in right angled traingle way.
-
 #NOTE: because i is multiplied into "*" it will be printed that many times we will be putting i, it means it will print 1 * in first row, as we get to second row it will print two times and so on.
