160170aa2e21345552bddf0eaa7b20d386054f85
Sathwikcm1 <sathwikcm@gmail.com>
2025-01-11 18:28:24 +0530

    7

--- 003_Arrays/001_Easy/007_Move_Zeroes_To_End.py
+++ 003_Arrays/001_Easy/007_Move_Zeroes_To_End.py
@@ -5,21 +5,23 @@
 #Copy the numbers to original array and fill the rest with zeroes.
 def brute_force(arr):
     n = len(arr)
-    temp = []
+    temp = [] #NOTE: This is the temp array.
     for i in range(n):
         if arr[i] != 0:
-            temp.append(arr[i])
-    print(temp)
-    temp_len = len(temp)
-    print(temp_len)
-    for i in range(temp_len):
+            temp.append(arr[i])#NOTE: putting all non-zero elements to the temporary array first.
+    temp_len = len(temp) #NOTE: Calculating the length of the temp array.
+    for i in range(temp_len): #NOTE: copying all the elements from temp to original array.
         arr[i] = temp[i]
-    print(arr)
-
+    #NOTE: Filling the remaining space in array with them zeroes.
     for i in range(temp_len,n):
         arr[i] = 0
+#TODO: Time complexity for this brute force approach:  O(2N)
 
-    print(arr)
+
+#TODO: Story of optimal: Two pointer method, alwasy try to shift rather putting it to another container.
+#So two pointers will be there i and j.
+def optimal(arr):
+    n=len(arr)
 
 arr = [1,2,3,4,0,2,0,2,3,0]
 print("The original array : ",arr)
