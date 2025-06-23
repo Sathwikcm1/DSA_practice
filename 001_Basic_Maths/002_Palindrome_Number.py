ea0f9216db5752df7f6aa597e1d51a4d2c14233a
Sathwikcm1 <sathwikcm@gmail.com>
2025-06-23 16:57:30 +0530

    some_changes

--- 001_Basic_Maths/002_Palindrome_Number.py
+++ 001_Basic_Maths/002_Palindrome_Number.py
@@ -7,14 +7,32 @@ def isPalindrome(x):
     if x < 0:
         return False
     rev = 0
+    if x >= 0 and x < 10:
+        return True
+    #NOTE: single-digit numbers (0-9) are palindrome since they are single-digit.
     copy = x
     while copy:
-        rev = rev*10 + copy % 10
+        last_digit = copy % 10
+        rev = rev * 10 + last_digit
         copy = copy//10
-    return x == rev or x == rev//10
+    return x == rev #NOTE: this is the comparision that is really happening.
 
 x = 121
 if isPalindrome(x):
     print("This is Palindrome.")
 else:
     print("This is not Palindrome.")
+
+
+#NOTE: or you can just convert the number into a string and then it as easy as this.
+
+def isPlanidrome_string_method(x):
+    if x < 0:
+        return False 
+    s = str(x)
+    return s == s[::-1]
+
+print("This is using the string method, the given number: 10901")
+if (isPlanidrome_string_method(10901)):
+    print("The given number is a palindrome.")
+
--- 001_Basic_Maths/003_Armstrong_Number.py
+++ 001_Basic_Maths/003_Armstrong_Number.py
@@ -2,26 +2,54 @@
 # the result will be the number itself
 # example: 371 = 3^3 + 7^3 + 1^3
 
-def is_Armstrong(n):
-    og = n 
-    #NOTE: this is the original number copy. since the original will change after that while loop.
-    sum = 0
-    while n:
-        #NOTE: Last digit is the mod of the original number.
-        ld = n % 10
-        #NOTE: sum is nothing but adding the cubes of each digit.
-        sum += ld ** 3 
-        n//=10
-
-        #NOTE: if the sum is equal to the original number we return true.
-    if og == sum:
-        return True
-    else:
+def basic_is_Armstrong(n):
+    if n < 0:
         return False
+    #NOTE: if the number is negative it is generally not considered for armstrong number.
+
+
+    og_n = n #NOTE: copying the original number.
+    num_digits = 0   #NOTE: This var will have the number of digits of the given number.
+    temp_n = n  #NOTE: temporary value to loop over again and again.
+
+    #HACK: counting the number of digits in the given number.
+    if temp_n == 0:
+        num_digits = 1
+    else:
+        while temp_n:
+            temp_n//=10
+            num_digits+=1
+
+    #HACK: Actually adding the cube of each number to see if we get the same result or not.
+    sum_of_powers = 0 #NOTE: this is the var that holds the result of the sum of powers.
+    temp_n = og_n #NOTE: re-initialising the temp to the original number.
+
+    while temp_n:
+        last_digit = temp_n % 10
+        sum_of_powers += last_digit ** num_digits
+        temp_n //= 10
+
+    return sum_of_powers == og_n
 
 
 n = 371
-if is_Armstrong(n):
+if basic_is_Armstrong(n):
     print("The given number is Armstrong")
 else:
     print("The given number is not Armstrong.")
+
+#TODO: if we really want to make this easy you alwasy convert the number into a string.
+
+def string_Armstrong(n):
+    if n < 0:
+        return False
+    s = str(n)
+    num_digits = len(s)
+
+    sum_of_powers = 0
+
+    for char_digits in s:
+        digit = int(char_digits)
+        sum_of_powers += digit ** num_digits
+
+    return n == sum_of_powers
