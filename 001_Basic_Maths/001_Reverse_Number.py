aef52c1851ae9bcafa7a46b9f55d539520230456
Sathwikcm1 <sathwikcm@gmail.com>
2026-01-21 09:08:38 +0530

    Leetcode Submits

--- 001_Basic_Maths/001_Reverse_Number.py
+++ 001_Basic_Maths/001_Reverse_Number.py
@@ -1,20 +1,45 @@
-#FIXME: Just return the reversed given number
+# FIXME: Just return the reversed given number
 # for example: x = 123 , return 321.
 
-#TODO: reversing a number, we take the last digit out of the number by doing modulus we get the last digit.
+# TODO: reversing a number, we take the last digit out of the number by doing modulus we get the last digit.
 # then we can add that to that rev_no variable which we initiated to zero at the beginning, if n < 0, we can still use the same function.
 def reverse_Num(n):
     rev_no = 0
-    if n < 0: #NOTE: handling the negative numbers part.
-        return -reverse_Num(-n) #TODO: handling this is easy, you send the negative of the result of the function. 
-        #NOTE: and we are feeding the same negative number to the function and then returning the negative of that result.
+    if n < 0:  # NOTE: handling the negative numbers part.
+        # TODO: handling this is easy, you send the negative of the result of the function.
+        return -reverse_Num(-n)
+        # NOTE: and we are feeding the same negative number to the function and then returning the negative of that result.
     while n:
-        ld = n % 10 #NOTE: last digit of the number.
-        rev_no = rev_no * 10 + ld #NOTE: reversing the number, 0*10+3, 3*10+2, 32*10+1 = 321.
-        n = n//10 #NOTE: Integer division is important here. 
+        ld = n % 10  # NOTE: last digit of the number.
+        # NOTE: reversing the number, 0*10+3, 3*10+2, 32*10+1 = 321.
+        rev_no = rev_no * 10 + ld
+        n = n//10  # NOTE: Integer division is important here.
     return rev_no if rev_no <= 0x7fffffff else 0
+
 
 n = -10
 ans = reverse_Num(n)
-print("The original number is : ",n)
-print("The reversed number is : ",ans)
+print("The original number is : ", n)
+print("The reversed number is : ", ans)
+
+
+# NOTE: In python there is no default integer size like it is there in c or c++ or java for int 32 bits and for long 64 bits, python handles it differently, arbitrary precision. the size allocated is arbitrary and grow as required.
+# so we don't need to write that 10th line in the actual code.
+#
+
+# TODO: Leetcode:
+
+Class Solution():
+    def reverse(self, n):
+        rev_no = 0
+        if n < 0:
+            return -self.reverse(-n)
+        elif n == 0:
+            return 0
+        while n:
+            ld = n % 10
+            rev_no = rev_no * 10 + ld
+            n //= 10
+        if rev_no < -2**31 or rev_no > 2**31 - 1:
+            return 0
+        return rev_no
--- 001_Basic_Maths/002_Palindrome_Number.py
+++ 001_Basic_Maths/002_Palindrome_Number.py
@@ -1,7 +1,7 @@
 
-#FIXME: determine whether an integer is a palindrome.
+# FIXME: determine whether an integer is a palindrome.
 
-#TODO: this is a simple algo, it's the same as reversing a number. 
+# TODO: this is a simple algo, it's the same as reversing a number.
 # if the original number is equal to the reversed number then the number is a palindrome otherwise return False.
 def isPalindrome(x):
     if x < 0:
@@ -9,13 +9,14 @@ def isPalindrome(x):
     rev = 0
     if x >= 0 and x < 10:
         return True
-    #NOTE: single-digit numbers (0-9) are palindrome since they are single-digit.
+    # NOTE: single-digit numbers (0-9) are palindrome since they are single-digit.
     copy = x
     while copy:
         last_digit = copy % 10
         rev = rev * 10 + last_digit
         copy = copy//10
-    return x == rev #NOTE: this is the comparision that is really happening.
+    return x == rev  # NOTE: this is the comparision that is really happening.
+
 
 x = 121
 if isPalindrome(x):
@@ -24,15 +25,35 @@ else:
     print("This is not Palindrome.")
 
 
-#NOTE: or you can just convert the number into a string and then it as easy as this.
+# NOTE: or you can just convert the number into a string and then it as easy as this.
 
 def isPlanidrome_string_method(x):
     if x < 0:
-        return False 
+        return False
     s = str(x)
     return s == s[::-1]
+
 
 print("This is using the string method, the given number: 10901")
 if (isPlanidrome_string_method(10901)):
     print("The given number is a palindrome.")
 
+
+# NOTE: Leetcode Program:
+
+Class Solution():
+    def isPalindrome(self, n):
+        if n < 0:
+            return False
+
+        if n >= 0 and n < 10:
+            return True
+
+        og = n
+        rev_no = 0
+        while n:
+            ld = n % 10
+            rev_no = rev_no * 10 + ld
+            n //= 10
+
+        return rev_no == og
